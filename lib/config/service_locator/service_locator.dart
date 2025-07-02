42a0ee5c9e410fe17a9dccde682ab5b28327cb07
developerSajib88 <developerSajib88@gmail.com>
2025-07-02 13:04:34 +0600

    :bug:[Fix]: Some project running issue fixed.

--- lib/config/service_locator/service_locator.dart
+++ lib/config/service_locator/service_locator.dart
@@ -1,13 +1,16 @@
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:learning_management/core/network/dio_client.dart';
+import 'package:learning_management/core/services/firebase_services/push_notification/push_notification_service.dart';
 
 final sl = GetIt.instance;
 
 void initServiceLocator(){
 
-  // sl.registerSingleton<DioClient>(DioClient());
-  // sl.registerSingleton<InternetConnectionChecker>(InternetConnectionChecker.instance);
-  // sl.registerSingleton<PushNotificationService>(PushNotificationServiceImpl());
-  //
+  sl.registerSingleton<DioClient>(DioClient());
+  sl.registerSingleton<InternetConnectionChecker>(InternetConnectionChecker.instance);
+  sl.registerSingleton<PushNotificationService>(PushNotificationServiceImpl());
+
   // /// Remote Services
   // sl.registerSingleton<HomeServices>(HomeServicesImpl());
   // sl.registerSingleton<RecipesService>(RecipesServiceImpl());
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:learning_management/config/service_locator/service_locator.dart';
 import 'package:learning_management/core/constants/local_database_keys.dart';
-import 'package:learning_management/core/services/firebase_services/push_notification/push_notification_service.dart';
 import 'package:learning_management/learning_management_app.dart';
 
 
@@ -16,7 +15,7 @@ void main()async{
   // await Firebase.initializeApp(
   //   options: DefaultFirebaseOptions.currentPlatform,
   // );
-  await sl<PushNotificationService>().initialize();
+  // await sl<PushNotificationService>().initialize();
 
   runApp(LearningManagementApp());
 }
