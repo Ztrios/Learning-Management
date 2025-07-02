94bf9148b9422a28bfaaaded04f2ffa9a9e2af40
developerSajib88 <developerSajib88@gmail.com>
2025-07-02 11:16:46 +0600

    :art:[Style]: App Text styles created.

--- lib/core/utils/styles/app_colors.dart
+++ lib/core/utils/styles/app_colors.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._(); // Private constructor to prevent instantiation
 
-  // Light Theme Colors
   static const Color primary = Color(0xFFF08320);
   // static const Color primary = Color(0xFF6200EA);
   // static const Color primaryVariant = Color(0xFF3700B3);
@@ -27,47 +26,5 @@ class AppColors {
 
   // Custom Colors
   static const Color splashBackground = Color(0xffC0DBBB);
-
-  // Dark Theme Colors
-  // static const Color darkBackground = Color(0xFF121212);
-  // static const Color darkSurface = Color(0xFF1E1E1E);
-  // static const Color darkOnPrimary = Colors.white;
-  // static const Color darkOnSecondary = Colors.white;
-  // static const Color darkOnBackground = Colors.white;
-  // static const Color darkOnSurface = Colors.white;
-  // static const Color darkOnError = Colors.black;
-
-
-  // /// Generate ThemeData for Light & Dark Mode
-  // static ThemeData lightTheme = ThemeData(
-  //   colorScheme: const ColorScheme.light(
-  //     primary: primary,
-  //     secondary: secondary,
-  //     background: background,
-  //     surface: surface,
-  //     error: error,
-  //     onPrimary: onPrimary,
-  //     onSecondary: onSecondary,
-  //     onBackground: onBackground,
-  //     onSurface: onSurface,
-  //     onError: onError,
-  //   ),
-  // );
-  //
-  //
-  // static ThemeData darkTheme = ThemeData(
-  //   colorScheme: const ColorScheme.dark(
-  //     primary: primary,
-  //     secondary: secondary,
-  //     background: darkBackground,
-  //     surface: darkSurface,
-  //     error: error,
-  //     onPrimary: darkOnPrimary,
-  //     onSecondary: darkOnSecondary,
-  //     onBackground: darkOnBackground,
-  //     onSurface: darkOnSurface,
-  //     onError: darkOnError,
-  //   ),
-  // );
 
 }
--- lib/core/utils/styles/app_text_styles.dart
+++ lib/core/utils/styles/app_text_styles.dart
@@ -1,64 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:learning_management/core/utils/styles/app_colors.dart';
 
 class AppTextStyles {
-  AppTextStyles._(); // Private constructor to prevent instantiation
+  const AppTextStyles._();
 
-  static final TextStyle heading = GoogleFonts.poppins(
-    fontSize: 25.sp,
-    fontWeight: FontWeight.w800,
-    color: AppColors.onHeading
-  );
-
-  static final TextStyle headline = GoogleFonts.poppins(
-    fontSize: 28.sp,
+  static const TextStyle heading1 = TextStyle(
+    fontSize: 32,
     fontWeight: FontWeight.bold,
-    color: AppColors.onHeadLine
+    color: Colors.black,
   );
 
-
-  static final TextStyle title = GoogleFonts.poppins(
-    fontSize: 20.sp,
-    fontWeight: FontWeight.w600, // Semibold
-    color: AppColors.onTitle
+  static const TextStyle heading2 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: Colors.black87,
   );
 
-  static final TextStyle titleWithOrangeColor = GoogleFonts.poppins(
-      fontSize: 20.sp,
-      fontWeight: FontWeight.w600, // Semibold
-      color: AppColors.primary
+  static const TextStyle subtitle = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: Colors.grey,
   );
 
-  static final TextStyle subtitle = GoogleFonts.poppins(
-    fontSize: 16.sp,
-    fontWeight: FontWeight.w500, // Medium
-    color: AppColors.onSubtitle
+  static const TextStyle body = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
   );
 
-  static final TextStyle bodyText = GoogleFonts.poppins(
-    fontSize: 15.sp,
-    fontWeight: FontWeight.w400, // Regular
-    color: AppColors.onBody
+  static const TextStyle caption = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Colors.grey,
   );
 
-  static final TextStyle caption = GoogleFonts.poppins(
-    fontSize: 12.sp,
-    fontWeight: FontWeight.w400, // Regular
-    color: AppColors.onCaptions
+  static const TextStyle button = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
   );
-
-  static final TextStyle button = GoogleFonts.poppins(
-    fontSize: 15.sp,
-    fontWeight: FontWeight.w500, // Medium
-    color: AppColors.surface
-  );
-
-  static final TextStyle buttonWithOrangeColor = GoogleFonts.poppins(
-      fontSize: 15.sp,
-      fontWeight: FontWeight.w500, // Medium
-      color: AppColors.primary
-  );
-
 }
