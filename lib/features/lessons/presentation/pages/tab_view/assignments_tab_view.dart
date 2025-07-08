0aff2259173b9fceb45d3c09223027bc7728c03e
developerSajib88 <developerSajib88@gmail.com>
2025-07-08 12:29:03 +0600

    :art:[Interface]: Assignment tab view ui design completed.

--- lib/features/lessons/presentation/pages/tab_view/assignments_tab_view.dart
+++ lib/features/lessons/presentation/pages/tab_view/assignments_tab_view.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:learning_management/features/lessons/presentation/widgets/item_view/assignments_item_view.dart';
 
 class AssignmentsTabView extends StatelessWidget {
   const AssignmentsTabView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      itemCount: 4,
+        itemBuilder: (context,index){
+          return AssignmentItemView(
+              title: "Assignment 1: Trigonometry",
+              isCompleted: index < 2,
+              status: index >= 2 ? "Missed" : "Submitted",
+          );
+        }
+    );
   }
 }
