216726d37c5d4f490b692d275be52e7e795cc20f
developerSajib88 <developerSajib88@gmail.com>
2025-07-10 18:15:25 +0600

    :art:[Feature]: Video player implemented for playing class videos.

--- lib/features/lessons/presentation/pages/tab_view/materials_tab_view.dart
+++ lib/features/lessons/presentation/pages/tab_view/materials_tab_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learning_management/features/lessons/presentation/widgets/item_view/materials_item_view.dart';
+import 'package:learning_management/features/lessons/presentation/widgets/video_player_widget.dart';
 
 class MaterialsTabView extends StatelessWidget {
   const MaterialsTabView({super.key});
@@ -9,7 +10,13 @@ class MaterialsTabView extends StatelessWidget {
     return ListView.separated(
       itemCount: 3,
         itemBuilder: (context, index){
-          return MaterialsItemView();
+          return InkWell(
+            onTap: (){
+              print("Hello I am called ");
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> VideoPlayerWidget()));
+            },
+            child: MaterialsItemView()
+          );
         },
       separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
