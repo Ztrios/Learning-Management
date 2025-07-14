8522f2ab5d945da531e6d8de99a2723bcf8a66a4
developerSajib88 <developerSajib88@gmail.com>
2025-07-14 11:50:29 +0600

    :art:[Functionality]: Uploading file type selection.

--- lib/features/lessons/presentation/widgets/files_upload_widget.dart
+++ lib/features/lessons/presentation/widgets/files_upload_widget.dart
@@ -1,5 +1,6 @@
 import 'package:defer_pointer/defer_pointer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:learning_management/core/utils/styles/app_colors.dart';
 import 'package:learning_management/core/utils/styles/app_text_styles.dart';
@@ -8,22 +9,28 @@ import 'package:learning_management/core/utils/ui_helpers/radius.dart';
 import 'package:learning_management/core/utils/ui_helpers/spacing.dart';
 import 'package:learning_management/features/lessons/presentation/widgets/submission_types_selection.dart';
 
-class FilesUploadWidget extends StatelessWidget {
+class FilesUploadWidget extends HookWidget {
   const FilesUploadWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final fileTypeIsPDF = useState<bool>(true);
+
     return DeferredPointerHandler(
       child: Column(
         children: [
-      
-          SubmissionTypeSelection(),
-      
+
+          SubmissionTypeSelection(
+            onChanged: (int index) => fileTypeIsPDF.value = index == 0,
+          ),
+
           gap24,
-      
+
           Row(
             children: [
-      
+
+              if(!fileTypeIsPDF.value)
               Expanded(
                 child: Container(
                   width: 1.sw,
@@ -39,14 +46,14 @@ class FilesUploadWidget extends StatelessWidget {
                     alignment: Alignment.center,
                     clipBehavior: Clip.none,
                     children: [
-      
+
                       Image.network(
                         "https://www.studiestoday.com/sites/default/files/images16/Revision%20Worksheet%20Class%206%20Mathematics_2.PNG",
                         width: 1.sw,
                         height: 1.sh,
                       ),
-      
-      
+
+
                       CircleAvatar(
                         backgroundColor: AppColors.deepOrange,
                         child: Text(
@@ -56,7 +63,7 @@ class FilesUploadWidget extends StatelessWidget {
                           ),
                         ),
                       ),
-      
+
                       Positioned(
                         top: -15,
                         right: -15,
@@ -75,13 +82,15 @@ class FilesUploadWidget extends StatelessWidget {
                           ),
                         ),
                       )
-      
-      
+
+
                     ],
                   ),
                 ),
               ),
-              gap12,
+
+              if(!fileTypeIsPDF.value) gap12,
+
               Expanded(
                 child: InkWell(
                   onTap: (){},
@@ -99,20 +108,20 @@ class FilesUploadWidget extends StatelessWidget {
                       mainAxisAlignment: mainCenter,
                       crossAxisAlignment: crossCenter,
                       children: [
-      
+
                         Icon(
                           Icons.add,
                           size: 45.sp,
                           color: AppColors.deepOrange,
                         ),
-      
+
                         Text(
                           "Click to Upload Photo",
                           style: AppTextStyles.bodySmall.copyWith(
                               color: AppColors.textPrimary
                           ),
                         )
-      
+
                       ],
                     ),
                   ),
--- lib/features/lessons/presentation/widgets/submission_types_selection.dart
+++ lib/features/lessons/presentation/widgets/submission_types_selection.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:learning_management/core/utils/styles/app_colors.dart';
 import 'package:learning_management/core/utils/ui_helpers/spacing.dart';
 import 'package:learning_management/widgets/buttons/primary_button.dart';
 
-class SubmissionTypeSelection extends StatefulWidget {
-  const SubmissionTypeSelection({super.key});
-
-  @override
-  State<SubmissionTypeSelection> createState() => _SubmissionTypeSelectionState();
-}
-
-class _SubmissionTypeSelectionState extends State<SubmissionTypeSelection> {
-  int selectedIndex = 0;
+class SubmissionTypeSelection extends HookWidget {
+  final Function(int indext) onChanged;
+  const SubmissionTypeSelection({
+    super.key,
+    required this.onChanged
+  });
 
   @override
   Widget build(BuildContext context) {
+
+    final selectedIndex = useState<int>(0);
+
     return Row(
       children: [
         // PDF Submission Tab
@@ -24,11 +25,12 @@ class _SubmissionTypeSelectionState extends State<SubmissionTypeSelection> {
             height: 35.h,
             child: PrimaryButton(
               onPressed: () {
-                setState(() => selectedIndex = 0);
+                selectedIndex.value = 0;
+                onChanged(0);
               },
               text: "PDF Submission",
-              background: selectedIndex == 0 ? AppColors.deepOrange : Colors.white,
-              textColor: selectedIndex == 0 ? Colors.white : AppColors.deepOrange,
+              background: selectedIndex.value == 0 ? AppColors.deepOrange : Colors.white,
+              textColor: selectedIndex.value == 0 ? Colors.white : AppColors.deepOrange,
               strokeColor: AppColors.deepOrange,
             ),
           ),
@@ -42,11 +44,12 @@ class _SubmissionTypeSelectionState extends State<SubmissionTypeSelection> {
             height: 35.h,
             child: PrimaryButton(
               onPressed: () {
-                setState(() => selectedIndex = 1);
+                selectedIndex.value = 1;
+                onChanged(1);
               },
               text: "Photo Submission",
-              background: selectedIndex == 1 ? AppColors.deepOrange : Colors.white,
-              textColor: selectedIndex == 1 ? Colors.white : AppColors.deepOrange,
+              background: selectedIndex.value == 1 ? AppColors.deepOrange : Colors.white,
+              textColor: selectedIndex.value == 1 ? Colors.white : AppColors.deepOrange,
               strokeColor: AppColors.deepOrange,
             ),
           ),
