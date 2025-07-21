566a1ddb001547b63020d0f533d7bddad78357aa
developerSajib88 <developersajib88@gmail.com>
2025-07-21 15:17:27 +0600

    :sparkles: [Bloc: Bloc providers created for all features.

--- lib/core/bloc/app_bloc_providers.dart
+++ lib/core/bloc/app_bloc_providers.dart
@@ -1,5 +1,13 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learning_management/features/authentication/presentation/bloc/authentication_bloc.dart';
+import 'package:learning_management/features/home/presentation/bloc/home_bloc.dart';
+import 'package:learning_management/features/lessons/presentation/bloc/lessions_bloc.dart';
+import 'package:learning_management/features/notifications/presentation/bloc/notification_bloc.dart';
+import 'package:learning_management/features/payments/presentation/bloc/payment_bloc.dart';
+import 'package:learning_management/features/profile/presentation/bloc/profile_bloc.dart';
+import 'package:learning_management/features/progress/presentation/bloc/progress_bloc.dart';
+import 'package:learning_management/features/results/presentation/bloc/results_bloc.dart';
+import 'package:learning_management/features/routine/presentation/bloc/routine_bloc.dart';
 
 class AppBlocProviders {
   const AppBlocProviders._();
@@ -7,6 +15,14 @@ class AppBlocProviders {
   /// All Bloc Providers are created here
   static List<BlocProvider> get providers => [
     //BlocProvider<InternetConnectivityCubit>(create: (context)=> InternetConnectivityCubit()..connectivityChecker()),
-    BlocProvider<AuthenticationBloc>(create: (context)=> AuthenticationBloc())
+    BlocProvider<AuthenticationBloc>(create: (context)=> AuthenticationBloc()),
+    BlocProvider<HomeBloc>(create: (context)=> HomeBloc()),
+    BlocProvider<LessionsBloc>(create: (context)=> LessionsBloc()),
+    BlocProvider<RoutineBloc>(create: (context)=> RoutineBloc()),
+    BlocProvider<ProgressBloc>(create: (context)=> ProgressBloc()),
+    BlocProvider<ResultsBloc>(create: (context)=> ResultsBloc()),
+    BlocProvider<NotificationsBloc>(create: (context)=> NotificationsBloc()),
+    BlocProvider<PaymentBloc>(create: (context)=> PaymentBloc()),
+    BlocProvider<ProfileBloc>(create: (context)=> ProfileBloc()),
   ];
 }
