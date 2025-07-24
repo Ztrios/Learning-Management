daf810f74ac2b1b00eb72f201b0a68d04dcab0d5
developerSajib88 <developerSajib88@gmail.com>
2025-07-24 17:19:49 +0600

    :sparkles: [Feat]: Student subject exam progress data inserted into exams progress cards.

--- lib/features/progress/presentation/widgets/cards/exams_progress_card.dart
+++ lib/features/progress/presentation/widgets/cards/exams_progress_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -8,6 +9,9 @@ import 'package:learning_management/core/utils/ui_helpers/alignments.dart';
 import 'package:learning_management/core/utils/ui_helpers/paddings.dart';
 import 'package:learning_management/core/utils/ui_helpers/radius.dart';
 import 'package:learning_management/core/utils/ui_helpers/spacing.dart';
+import 'package:learning_management/features/progress/data/models/progress_models.dart';
+import 'package:learning_management/features/progress/presentation/bloc/progress_bloc.dart';
+import 'package:learning_management/features/progress/presentation/bloc/progress_event.dart';
 
 import '../progress_indicator_circle.dart';
 
@@ -16,113 +20,119 @@ class ExamsProgressCard extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: padding16,
-      decoration: BoxDecoration(
-        borderRadius: radius16,
-        color: AppColors.blueLight,
-      ),
-      child: Column(
-        crossAxisAlignment: crossCenter,
-        mainAxisAlignment: mainCenter,
-        children: [
-          _LanguageDropdown(),
-          gap12,
-          Text(
-            "Your result ",
-            style: AppTextStyles.titleLarge.copyWith(
-                color: Colors.white,
-                fontWeight: FontWeight.bold
-            ),
+    return BlocBuilder<ProgressBloc, ProgressState>(
+      builder: (context, state) {
+        return Container(
+          padding: padding16,
+          decoration: BoxDecoration(
+            borderRadius: radius16,
+            color: AppColors.blueLight,
           ),
-          gap24,
-          ProgressIndicatorCircle(
-            percantage: 72,
-            message: "Good progress",
-            progressColor: AppColors.deepOrange,
-            isForExam: true,
+          child: Column(
+            crossAxisAlignment: crossCenter,
+            mainAxisAlignment: mainCenter,
+            children: [
+              LanguageDropdown(),
+              gap12,
+              Text(
+                "Your result ",
+                style: AppTextStyles.titleLarge.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              gap24,
+              ProgressIndicatorCircle(
+                percantage: state.selectedSubject?.examPercentage ?? 0,
+                message: "Good progress",
+                progressColor: AppColors.deepOrange,
+                isForExam: true,
+              ),
+              gap24,
+              _StatsCards(),
+            ],
           ),
-          gap24,
-          _StatsCards(),
-        ],
-      ),
+        );
+      },
     );
   }
 }
 
-class _LanguageDropdown extends HookWidget {
+class LanguageDropdown extends HookWidget {
+  const LanguageDropdown({super.key,});
+
   @override
   Widget build(BuildContext context) {
-
-
-    final selectedSubject = useState<String>("English");
-
-    const List<String> subjects = [
-      "English",
-      "Bangla",
-      "History",
-      "Geography",
-      "Math"
-    ];
-
-    return Row(
-      mainAxisAlignment: mainEnd,
-      children: [
-        Container(
-          padding: paddingH12,
-          decoration: BoxDecoration(
-            borderRadius: radius12,
-            color: Colors.white
-          ),
-          child: DropdownButtonHideUnderline(
-            child: DropdownButton<String>(
-              value: selectedSubject.value,
-              icon: Icon(Icons.keyboard_arrow_down, color: AppColors.deepBlue,
-                  size: 25.sp),
-              dropdownColor: Colors.white,
-              borderRadius: BorderRadius.circular(10.r),
-              style: AppTextStyles.titleSmall.copyWith(fontWeight: FontWeight.bold),
-              onChanged: (value) {
-                if (value != null) {
-                    selectedSubject.value = value;
-                }
-              },
-              items: subjects.map((String subject) {
-                return DropdownMenuItem<String>(
-                  value: subject,
-                  child: Text(subject),
-                );
-              }).toList(),
-            ),
-          ),
-        )
-      ],
+    return BlocBuilder<ProgressBloc, ProgressState>(
+      builder: (context, state) {
+        return Row(
+          mainAxisAlignment: mainEnd,
+          children: [
+            Container(
+              padding: paddingH12,
+              decoration: BoxDecoration(
+                  borderRadius: radius12,
+                  color: Colors.white
+              ),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<int>(
+                  value: state.selectedSubject?.subjectId,
+                  icon: Icon(
+                      Icons.keyboard_arrow_down, color: AppColors.deepBlue,
+                      size: 25.sp),
+                  dropdownColor: Colors.white,
+                  borderRadius: BorderRadius.circular(10.r),
+                  style: AppTextStyles.titleSmall.copyWith(
+                      fontWeight: FontWeight.bold),
+                  onChanged: (value) {
+                    if (value != null) {
+                      context.read<ProgressBloc>().add(
+                          SelectSubject(subjectId: value));
+                    }
+                  },
+                  items: state.progressEntity?.subjectProgresses?.map((
+                      subjectProgress) {
+                    return DropdownMenuItem<int>(
+                      value: subjectProgress.subjectId,
+                      child: Text(subjectProgress.subjectName ?? ""),
+                    );
+                  }).toList(),
+                ),
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
-
 
 
 class _StatsCards extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      spacing: 16.w,
-      children: [
-        _StatCard(
-          value: "32",
-          label: "Quiz Attended",
-          backgroundColor: Colors.white,
-          textColor: Colors.black,
-        ),
-        _StatCard(
-          value: "21",
-          label: "Assignment Done",
-          backgroundColor: AppColors.deepOrange,
-          textColor: Colors.white,
-          iconPath: "assets/icons/score_icon.svg",
-        ),
-      ],
+    return BlocBuilder<ProgressBloc, ProgressState>(
+      builder: (context, state) {
+        return Row(
+          spacing: 16.w,
+          children: [
+            _StatCard(
+              value: (state.selectedSubject?.totalExams ?? 00).toString(),
+              label: "Total Exams",
+              backgroundColor: Colors.white,
+              textColor: Colors.black,
+            ),
+            _StatCard(
+              value: (state.selectedSubject?.totalExamAttends ?? 00).toString(),
+              label: "Completed",
+              backgroundColor: AppColors.deepOrange,
+              textColor: Colors.white,
+              iconPath: "assets/icons/score_icon.svg",
+            ),
+          ],
+        );
+      },
     );
   }
 }
