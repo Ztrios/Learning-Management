5de838d61e0aac633802effabb881884731df84f
developerSajib88 <developerSajib88@gmail.com>
2025-07-24 17:58:24 +0600

    :sparkles: [Feat]: Loading animation added in Progress card.

--- lib/features/progress/presentation/pages/progress_page.dart
+++ lib/features/progress/presentation/pages/progress_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:learning_management/core/utils/extensions/status_extension.dart';
 import 'package:learning_management/core/utils/styles/app_colors.dart';
 import 'package:learning_management/core/utils/ui_helpers/paddings.dart';
 import 'package:learning_management/core/utils/ui_helpers/spacing.dart';
@@ -13,6 +14,9 @@ import 'package:learning_management/features/progress/presentation/widgets/cards
 import 'package:learning_management/features/progress/presentation/widgets/cards/quiz_progress_card.dart';
 import 'package:learning_management/features/progress/presentation/widgets/stats_header_widget.dart';
 import 'package:learning_management/features/progress/presentation/widgets/tab_bar/progress_tab_bar.dart';
+import 'package:learning_management/features/results/presentation/bloc/results_bloc.dart';
+import 'package:learning_management/features/results/presentation/bloc/results_event.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class ProgressPage extends HookWidget {
 
@@ -35,6 +39,7 @@ class ProgressPage extends HookWidget {
       int? studentId = context.read<AuthBloc>().state.signInEntity?.signInData?.student?.id;
       if(studentId != null){
         context.read<ProgressBloc>().add(GetStudentProgress(studentId: studentId.toString()));
+        context.read<ResultsBloc>().add(GetStudentResults(studentId: studentId.toString()));
       }
     }
 
@@ -55,25 +60,39 @@ class ProgressPage extends HookWidget {
             children: [
               gap24,
 
-              StatsHeader(
-                grade: "3.33",
-                totalRank: "1,438",
-                classRank: "56",
-                background: selectedIndex.value == 0
-                    ? AppColors.blueLight
-                    : selectedIndex.value == 1 ?
-                AppColors.deepPurpleAccent : AppColors.tealBlue,
+              BlocBuilder<ResultsBloc, ResultsState>(
+                builder: (context, state) {
+                  return Skeletonizer(
+                    enabled: state.status.isLoading,
+                    child: StatsHeader(
+                      grade: state.resultsEntity?.resultsData?.overallGrade ?? "0.00",
+                      totalRank: (state.resultsEntity?.resultsData?.standardRank ?? 00).toString(),
+                      classRank: (state.resultsEntity?.resultsData?.sectionRank ?? 00).toString(),
+                      background: selectedIndex.value == 0
+                          ? AppColors.blueLight
+                          : selectedIndex.value == 1 ?
+                      AppColors.deepPurpleAccent : AppColors.tealBlue,
+                    ),
+                  );
+                },
               ),
 
-              Column(
-                children: [
-                  ProgressTabBar(
-                    selectedIndex: selectedIndex.value,
-                    tabs: ["Exams", "Quiz", "Assignment"],
-                    onChanged: (int value) => selectedIndex.value = value,
-                  ),
-                  progressCads[selectedIndex.value],
-                ],
+              BlocBuilder<ProgressBloc, ProgressState>(
+                builder: (context, state) {
+                  return Skeletonizer(
+                    enabled: state.status.isLoading,
+                    child: Column(
+                      children: [
+                        ProgressTabBar(
+                          selectedIndex: selectedIndex.value,
+                          tabs: ["Exams", "Quiz", "Assignment"],
+                          onChanged: (int value) => selectedIndex.value = value,
+                        ),
+                        progressCads[selectedIndex.value],
+                      ],
+                    ),
+                  );
+                },
               )
 
 
