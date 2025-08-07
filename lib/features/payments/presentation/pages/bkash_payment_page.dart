05781866003f9a6334e798291df323608d2d5f18
developerSajib88 <developerSajib88@gmail.com>
2025-08-07 18:01:16 +0600

    :sparkles: [Feat][Payments]: Bkash payment interface.

--- lib/features/payments/presentation/pages/bkash_payment_page.dart
+++ lib/features/payments/presentation/pages/bkash_payment_page.dart
@@ -1,6 +1,14 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:learning_management/core/constants/api_urls.dart';
+import 'package:learning_management/core/helpers/toast_notification/toast_notifications.dart';
+import 'package:learning_management/core/utils/styles/app_colors.dart';
+import 'package:learning_management/features/payments/presentation/pages/payment_page.dart';
+import 'package:learning_management/widgets/circle_loading.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class BkashPaymentPage extends HookWidget {
@@ -11,6 +19,7 @@ class BkashPaymentPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
 
+    final isLoading = useState<bool>(true);
     final webViewController = useMemoized(() => WebViewController());
 
     void webViewInitializing(){
@@ -19,18 +28,36 @@ class BkashPaymentPage extends HookWidget {
         ..setNavigationDelegate(
           NavigationDelegate(
               onPageStarted: (_) => webViewController.clearCache(),
-              onPageFinished: (String url) {},
+              onProgress: (value){
+                print(value);
+                if(value >= 100) isLoading.value = false;
+              } ,
+              // onWebResourceError: (WebResourceError error) {
+              //   ToastNotifications.showErrorToast(
+              //     title: "Payment Failed",
+              //     message: "We couldn't complete your transaction. Please try again or use a different method.",
+              //   );
+              // },
               onNavigationRequest: (NavigationRequest request) {
 
-                return NavigationDecision.navigate;
-              },
+                  log("Navigation: ${request.url}");
+
+                  if(request.url.startsWith(ApiUrls.baseURL + ApiUrls.verifyPayment)){
+                    final queryParams = Uri.parse(request.url).queryParameters;
+                    if(queryParams["status"] == "success"){
+                      ToastNotifications.showSuccessToast("Thank you! Your payment is completed.");
+                    }else if(queryParams["status"] == "cancel"){
+                      ToastNotifications.showErrorToast(
+                        title: "Incomplete Payment",
+                        message: "Your payment is not completed."
+                      );
+                    }
+                    context.pushReplacementNamed(PaymentPage.name);
+                  }
+
+                  return NavigationDecision.navigate;
+                },
           ),
-        )
-        ..addJavaScriptChannel(
-          "onPaymentSuccess",
-          onMessageReceived: (JavaScriptMessage message) {
-            Navigator.pop(context);
-          },
         )
         ..loadRequest(Uri.parse(bkashUrl)); // Use passed URL
     }
@@ -41,9 +68,29 @@ class BkashPaymentPage extends HookWidget {
     }, [webViewController]);
 
     return Scaffold(
-      appBar: AppBar(title: Text("Bkash Payment")),
-      body: WebViewWidget(
-          controller: webViewController
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.arrow_back_ios, color: Colors.white)
+        ),
+        title: Text("Bkash Payment", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent,
+      ),
+      backgroundColor: AppColors.background,
+      body: Container(
+        width: 1.sw,
+        height: 1.sh,
+        alignment: Alignment.center,
+        child: Visibility(
+          visible: !isLoading.value,
+          replacement: CircleLoadingWidget(),
+          child: Center(
+            child: WebViewWidget(
+                controller: webViewController
+            ),
+          ),
+        ),
       ),
     );
   }
