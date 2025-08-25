75ab8f91cc678a5ef75c41146561ef53557211d3
HelloSajib <sajibhasan.dev@gmail.com>
2025-08-25 17:44:11 +0600

    :art: [feat][auth]: User account delete completed.

--- lib/widgets/dialogs/account_delete_dialog.dart
+++ lib/widgets/dialogs/account_delete_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:learning_management/core/helpers/toast_notification/toast_notifications.dart';
 import 'package:learning_management/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:learning_management/features/auth/presentation/bloc/auth_event.dart';
 import 'package:learning_management/features/auth/presentation/pages/sign_in_page.dart';
@@ -49,6 +50,11 @@ Future<void> showAccountDeleteDialog(BuildContext context) async {
                 context.read<AuthBloc>().add(DiactivateAccount(studentId: studentId.toString()));
                 context.go(SplashPage.path);
                 Navigator.pop(context);
+                ToastNotifications.showErrorToast(
+                  title: "Account Deleted",
+                  message: "Your account and all associated data have been permanently removed.",
+                  alignment: Alignment.topCenter,
+                );
               }
             },
             child: Text('Delete', style: TextStyle(color: Colors.white),),
